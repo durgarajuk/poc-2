9982ec29e7a41eacb54bb214debda01ded39df23
durgarajuk <55746298+durgarajuk@users.noreply.github.com>
2026-03-27 18:26:25 +0530

    Updatedname Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Pipeline is fully updated");
+        System.out.println("Hello, this is a live demo!");
     }
 }
