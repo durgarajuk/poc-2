80ee542f01c721bb9df79c3d3745434a9341d7db
durgarajuk <55746298+durgarajuk@users.noreply.github.com>
2026-03-27 18:03:37 +0530

    Latest Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, this is a live demo of durgaraju");
+        System.out.println("Hello, Pipeline is fully automated!");
     }
 }
