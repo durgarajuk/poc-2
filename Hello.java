d721b8ae587cd82c3252c97184b2d290975232b2
durgarajuk <55746298+durgarajuk@users.noreply.github.com>
2026-03-28 21:27:23 +0530

    Updatddde Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Pipeline Demo Success");
+        System.out.println("Pipeline Demo Succes");
     }
 }
